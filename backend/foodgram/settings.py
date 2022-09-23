c998282c25be3fffc5c0a12ab7b8ac3e3dab5698
Zorik Karapetyan <zorikkarapetan@gmail.com>
2022-09-23 11:12:10 +0300

    docker front

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='testkey')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
