171112f88bcb2b27e3d186838f605b2050dcc920
Zorik Karapetyan <zorikkarapetan@gmail.com>
2022-09-30 11:39:28 +0300

    111

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'foodgram.middleware.Process500'
+    # 'foodgram.middleware.Process500'
 ]
 
 ROOT_URLCONF = 'foodgram.urls'
