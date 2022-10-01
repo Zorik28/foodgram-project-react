0a1b16d54b0ed40941e9e003743a2c1323cfa877
Zorik Karapetyan <zorikkarapetan@gmail.com>
2022-10-02 00:36:49 +0400

    pep8

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
-from rest_framework.status import (HTTP_201_CREATED, HTTP_204_NO_CONTENT,
-                                   HTTP_400_BAD_REQUEST)
+from rest_framework.status import (
+    HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
+)
 from rest_framework.viewsets import ModelViewSet
 
 from recipes.models import IngredientInRecipe, Recipe
